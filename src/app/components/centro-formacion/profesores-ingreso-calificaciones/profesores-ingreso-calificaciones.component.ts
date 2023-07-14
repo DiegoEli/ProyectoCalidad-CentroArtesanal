acc715e31b5ac7459c759b47b9b645f93b9b5294
Hidalgo1996 <GeForce210_1996@hotmail.com>
2023-07-13 19:31:50 -0500

    Vista de registro calificaciones

--- src/app/components/centro-formacion/profesores-ingreso-calificaciones/profesores-ingreso-calificaciones.component.ts
+++ src/app/components/centro-formacion/profesores-ingreso-calificaciones/profesores-ingreso-calificaciones.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./profesores-ingreso-calificaciones.component.css']
 })
 export class ProfesoresIngresoCalificacionesComponent {
+  estudiante: string = ''; // Variable para almacenar el estudiante seleccionado
+  parcial1: number = 0; // Variable para almacenar la nota del primer parcial
+  parcial2: number = 0; // Variable para almacenar la nota del segundo parcial
+  modulo: string = ''; // Variable para almacenar el módulo seleccionado
+  promedio: number = 0; // Variable para almacenar el promedio calculado
+  calificaciones: any[] = []; // Array para almacenar las calificaciones registradas
+
+  calcularPromedio(): void {
+    this.promedio = (this.parcial1 + this.parcial2) / 2;
+  }
+  registrarCalificaciones(): void {
+    // Crear un objeto con los datos de la calificación
+    const calificacion = {
+      estudiante: this.estudiante,
+      parcial1: this.parcial1,
+      parcial2: this.parcial2,
+      promedio: this.promedio,
+      modulo: this.modulo
+    };
+
+    // Agregar la calificación al array de calificaciones
+    this.calificaciones.push(calificacion);
+
+    // Limpiar los campos de entrada
+    this.estudiante = '';
+    this.parcial1 = 0;
+    this.parcial2 = 0;
+    this.modulo = '';
+    this.promedio = 0;
+  }
 
 }
